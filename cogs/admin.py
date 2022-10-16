5de27ee19ae8298701290247e89020e551cf2a24
Dave <titanicboyke@telenet.be>
2022-10-16 08:08:21 +0200

    Update admin.py
    Elite Raid + Elite Raid Egg

--- cogs/admin.py
+++ cogs/admin.py
@@ -75,7 +75,7 @@ class Admin(commands.Cog):
         level_list = list(levels.split(','))
         level_list = list(map(int, level_list))
 
-        if all(i > 8 or i < 1 for i in level_list):
+        if all(i > 9 or i < 1 for i in level_list):
             embed.description = "Couldn't create Raid Board. Try chosing other levels."
             await message.edit(embed=embed)
             return
@@ -111,7 +111,7 @@ class Admin(commands.Cog):
         level_list = list(levels.split(','))
         level_list = list(map(int, level_list))
 
-        if all(i > 8 or i < 1 for i in level_list):
+        if all(i > 9 or i < 1 for i in level_list):
             embed.description = "Couldn't create Egg Board. Try chosing other levels."
             await message.edit(embed=embed)
             return
@@ -175,7 +175,10 @@ class Admin(commands.Cog):
                         stats.append("raid_lvl_7_active")
                     elif "8" in stat:
                         stats.append("raid_lvl_8_active")
+                    elif "9" in stat:
+                        stats.append("raid_lvl_9_active")
                     elif "all" in stat:
+                        stats.append("raid_lvl_9_active")
                         stats.append("raid_lvl_8_active")
                         stats.append("raid_lvl_7_active")
                         stats.append("raid_lvl_6_active")
@@ -204,7 +207,10 @@ class Admin(commands.Cog):
                         stats.append("egg_lvl_7_active")
                     elif "8" in stat:
                         stats.append("egg_lvl_8_active")
+                    elif "9" in stat:
+                        stats.append("egg_lvl_9_active")
                     elif "all" in stat:
+                        stats.append("egg_lvl_9_active")
                         stats.append("egg_lvl_8_active")
                         stats.append("egg_lvl_7_active")
                         stats.append("egg_lvl_6_active")
